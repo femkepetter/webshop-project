f02656480282caaee49046871c51b6d04819e4c1
NinaAurora <ninika@gmail.com>
2021-06-23 14:56:37 +0200

    Added css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,74 @@
-@include('layouts.components.header')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<div id="app">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    @include('layouts.components.navbar')
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <main class="py-4">
-        @yield('content')
-    </main>
-</div>
-<!-- Scripts -->
-<script src="{{ asset('js/app.js') }}"></script>
-@stack('child-script')
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-@include('layouts.components.footer')
+    <!-- BOOTSTRAP -->
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+    <!-- ANIMATIONS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
+    <!-- FONTS -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,500;0,600;0,700;0,800;0,900;1,200;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+    <div id="app">
+
+        <div class="container-fluid">
+
+            @include('layouts.components.navbar')
+
+            <main class="py-4">
+                @yield('content')
+            </main>
+        </div>
+
+    </div>
+    <!-- Scripts -->
+
+    @stack('child-script')
+
+    <div class="overflow-hidden">
+        <div class="circle-1"></div>
+        <div class="circle-2"></div>
+        <div class="circle-3"></div>
+        <div class="circle-4"></div>
+    </div>
+
+
+    <script>
+        $(document).ready(function () {
+            document.getElementById("liveToastBtn").onclick = function () {
+                $('.toast').toast('show');
+            }
+        })
+
+        var myCarousel = document.querySelector('#myCarousel')
+        var carousel = new bootstrap.Carousel(myCarousel, {
+            interval: 2000,
+            wrap: false
+        })
+
+    </script>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>

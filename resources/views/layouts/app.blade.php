0100e1aaa0930878e3d3e7d9e7fdd72a835f206b
Femke Petter <fnpetter@gmail.com>
2021-06-23 14:39:41 +0200

    merged from front-end

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,39 +1,15 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@include('layouts.components.header')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<div id="app">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    @include('layouts.components.navbar')
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <main class="py-4">
+        @yield('content')
+    </main>
+</div>
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+@stack('child-script')
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                
-                <span>Navbar</span>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    @stack('child-script')
-</body>
-
-</html>
+@include('layouts.components.footer')

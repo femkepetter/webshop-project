8552d37572f62f262a4664f485a8ef5879fc8b13
NinaAurora <ninika@gmail.com>
2021-06-24 09:39:21 +0200

    fixed app and show.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,75 +30,44 @@
 
     <div id="app">
 
-        @include('layouts.components.navbar')
+        <div class="container-fluid">
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+            @include('layouts.components.navbar')
+
+            <main class="py-4">
+                @yield('content')
+            </main>
+        </div>
+
     </div>
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+
     @stack('child-script')
 
-    <!-- BOOTSTRAP -->
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-
-    <!-- ANIMATIONS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-
-    <!-- FONTS -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,500;0,600;0,700;0,800;0,900;1,200;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-        rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
-
-    <body>
-
-        <div id="app">
-
-            <div class="container-fluid">
-
-                @include('layouts.components.navbar')
-
-                <main class="py-4">
-                    @yield('content')
-                </main>
-            </div>
-
-        </div>
-        <!-- Scripts -->
-
-        @stack('child-script')
-
-        <div class="overflow-hidden">
-            <div class="circle-1"></div>
-            <div class="circle-2"></div>
-            <div class="circle-3"></div>
-            <div class="circle-4"></div>
-        </div>
+    <div class="overflow-hidden">
+        <div class="circle-1"></div>
+        <div class="circle-2"></div>
+        <div class="circle-3"></div>
+        <div class="circle-4"></div>
+    </div>
 
 
-        <script>
-            $(document).ready(function () {
-                document.getElementById("liveToastBtn").onclick = function () {
-                    $('.toast').toast('show');
-                }
-            })
+    <script>
+        $(document).ready(function () {
+            document.getElementById("liveToastBtn").onclick = function () {
+                $('.toast').toast('show');
+            }
+        })
 
-            var myCarousel = document.querySelector('#myCarousel')
-            var carousel = new bootstrap.Carousel(myCarousel, {
-                interval: 2000,
-                wrap: false
-            })
+        var myCarousel = document.querySelector('#myCarousel')
+        var carousel = new bootstrap.Carousel(myCarousel, {
+            interval: 2000,
+            wrap: false
+        })
 
-        </script>
+    </script>
 
-        <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
 </html>
 </body>

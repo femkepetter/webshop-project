2e42f22d4d5572a36b2dbd4dbc0d6f5b9a7d0679
NinaAurora <ninika@gmail.com>
2021-06-24 15:17:41 +0200

    updated the category page

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -5,11 +5,32 @@
 @if ($products->count() > 0)
 <div class="row px-5 gx-5">
 @foreach ($products as $product)
-    <div class="col-lg-4">
-        <img src="https://picsum.photos/300" alt="placeholder">
-            <a href="{{ route('product.show', $product) }}">
-                <p>{{ $product->name }}</p>
-            </a>
+<div class="col-md-6 col-xl-4">
+        <img src="https://picsum.photos/320/250" alt="" class="img-fluid">
+            <div class="row my-2">
+                <div class="col-sm-8 col-lg-9  my-auto mt-2">
+                    <a href="{{ route('product.show', $product) }}">
+                        <h3>{{ $product->name }} ${{ $product->price }}</h3>
+                    </a>
+                </div>
+                <div class="col-sm-12 col-md-3 col-lg-3 text-center">
+                    <button type="button" class="btn btn-cart " id="${{ $product->id }}"><i
+                            class="bi bi-bag-plus-fill hvr-grow"></i></button>
+
+                    <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 5">
+                        <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
+                            <div class="toast-header">
+                                <img src="..." class="rounded me-2" alt="...">
+                                <strong class="me-auto">Bootstrap</strong>
+                                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                            </div>
+                            <div class="toast-body">
+                                Added product to cart
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </div>
     @endforeach
 

a4ff8e766db0cea971c0c539848073ce40eb35e4
NinaAurora <ninika@gmail.com>
2021-07-07 15:47:33 +0200

    Fixed alignment

--- resources/views/cart/show.blade.php
+++ resources/views/cart/show.blade.php
@@ -13,36 +13,36 @@
  
         </tr>
     </thead>
-    <tbody>
-        @php $total = 0 @endphp
-        @if(session('cart'))
-            @foreach(session('cart') as $id => $details)
-                @php $total += $details['price'] * $details['quantity'] @endphp
-                <tr data-id="{{ $id }}">
-                    <td data-th="Product">
-                        <div class="row">
-                            <div class="col-sm-9">
-                                <h4 class="nomargin">{{ $details['name'] }}</h4>
+        <tbody>
+            @php $total = 0 @endphp
+            @if(session('cart'))
+                @foreach(session('cart') as $id => $details)
+                    @php $total += $details['price'] * $details['quantity'] @endphp
+                    <tr data-id="{{ $id }}">
+                        <td data-th="Product">
+                            <div class="row">
+                                <div class="col-sm-9">
+                                    <h4 class="nomargin">{{ $details['name'] }}</h4>
+                                </div>
                             </div>
-                        </div>
-                    </td>
-                    <td data-th="Price">${{ $details['price'] }}</td>
-                    <td data-th="Quantity">
-                        <input type="number" value="{{ $details['quantity'] }}" class="form-control quantity update-cart" />
-                    </td>
-                    <td class="actions">
-                        <button class="btn btn-cart btn-sm remove-from-cart"><i class="bi bi-x"></i></button>
-                    </td>
-                    <td data-th="Subtotal" class="text-end">${{ $details['price'] * $details['quantity'] }}</td>
-                </tr>
-            @endforeach
-        @endif
-    </tbody>
-    <tfoot>
-        <tr>
-            <td colspan="5" class="text-end"><h3><strong>Subtotal: ${{ $total }}</strong></h3></td>
-        </tr>
-    </tfoot>
+                        </td>
+                        <td data-th="Price">${{ $details['price'] }}</td>
+                        <td data-th="Quantity">
+                            <input type="number" value="{{ $details['quantity'] }}" class="form-control quantity update-cart" />
+                        </td>
+                        <td class="actions">
+                            <button class="btn btn-cart btn-sm remove-from-cart"><i class="bi bi-x"></i></button>
+                        </td>
+                        <td data-th="Subtotal" class="text-end">${{ $details['price'] * $details['quantity'] }}</td>
+                    </tr>
+                @endforeach
+            @endif
+        </tbody>
+        <tfoot>
+            <tr>
+                <td colspan="5" class="text-end"><h3><strong>Subtotal: ${{ $total }}</strong></h3></td>
+            </tr>
+        </tfoot>
 </table>
 </div>
 
